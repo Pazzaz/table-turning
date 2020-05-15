cc394ec93b994d5d8d03dd63a5bacfd49a495c67
Pazzaz <pazzaz.sundqvist@gmail.com>
2020-05-15 21:50:58 +0200

    Add the ability to see the path traversed to find the target

--- find/src/main.rs
+++ find/src/main.rs
@@ -1,13 +1,14 @@
 
 use std::collections::HashSet;
+use std::collections::HashMap;
 
-const N: usize = 5;
-const PHI: usize = 6;
+const N: usize = 6;
+const PHI: usize = 5;
 
 fn traverse(starting_node: (Vec<u8>, u8), classes: &Vec<usize>) -> bool {
-    let mut visited: HashSet<(u64, u8)> = HashSet::new();
+    let mut visited: HashMap<(u64, u8), Option<((u64, u8), u8)>> = HashMap::new();
     let nnn = to_number(&starting_node.0);
-    visited.insert((nnn, starting_node.1));
+    visited.insert((nnn, starting_node.1), None);
     let mut stack = Vec::new();
     let neigh = neighbours(&starting_node, &mut visited, &classes);
     stack.push((neigh, 0));
@@ -25,6 +26,7 @@ fn traverse(starting_node: (Vec<u8>, u8), classes: &Vec<usize>) -> bool {
 
         let new_current = &top.0[top.1];
         if new_current.0.len() == 0 {
+            find_history(visited, new_current.clone());
             return true;
         }
         let new_neigh = neighbours(new_current, &mut visited, &classes);
@@ -32,11 +34,36 @@ fn traverse(starting_node: (Vec<u8>, u8), classes: &Vec<usize>) -> bool {
         stack.push((new_neigh, 0));
         i += 1;
     }
+}
 
+fn find_history(visited: HashMap<(u64, u8), Option<((u64, u8), u8)>>, end_node: (Vec<u8>, u8)) {
+    let conv = to_number(&end_node.0);
+    let mut conv_tup = (conv, end_node.1);
+    let (mut node, mut mov) = visited.get(&conv_tup).unwrap().unwrap();
+    println!("SO YOU WANT TO ESCAPE FROM YOUR LAIR? WELL JUST FOLLOW THESE INSTRUCTIONS");
+    let mut out = Vec::new();
+    out.push(mov);
+
+    loop {
+        let tup = *visited.get(&node).unwrap();
+        match tup {
+            Some((a, b)) => {
+                node = a;
+                mov = b;
+            },
+            None => break,
+        }
+        out.push(mov)
+    }
+    out.reverse();
+    for o in out {
+        println!("{:06b}", o);
+    }
 }
 
 
-fn neighbours(node: &(Vec<u8>, u8), visited: &mut HashSet<(u64, u8)>, classes: &Vec<usize>) -> Vec<(Vec<u8>, u8)> {
+
+fn neighbours(node: &(Vec<u8>, u8), visited: &mut HashMap<(u64, u8), Option<((u64, u8), u8)>>, classes: &Vec<usize>) -> Vec<(Vec<u8>, u8)> {
     let mut out: Vec<(Vec<u8>, u8)> = Vec::new();
     let max = if node.1 == 0 {
         classes.len()
@@ -45,23 +72,24 @@ fn neighbours(node: &(Vec<u8>, u8), visited: &mut HashSet<(u64, u8)>, classes: &
     };
     for k in 0..max {
         let i = if node.1 == 0 {
-            classes[k]
+            classes[k] 
         } else {
             k
         };
-        let mut flipped = move_it(&node.0, i as u8);
+        let i = i as u8;
+        let mut flipped = move_it(&node.0, i);
 
         let mut next_round = node.1 + 1;
         if next_round == (PHI as u8) {
             flipped = mix_around(&flipped);
             next_round = 0;
         }
-        let mut tup = (flipped, next_round);
-        if !perms_visited(tup.0.clone(), next_round, visited) {
-            let nnn = to_number(&tup.0);
-            visited.insert((nnn, next_round));
-            tup.0.sort();
-            out.push(tup);
+        if !perms_visited(&mut flipped, next_round, visited) {
+            let nnn = to_number(&flipped);
+            let orig_nnn = to_number(&node.0);
+            visited.insert((nnn, next_round), Some(((orig_nnn, node.1), i)));
+            flipped.sort();
+            out.push((flipped, next_round));
         }
     }
     out
@@ -79,10 +107,11 @@ fn swapper(p: usize, q: usize, values: &mut Vec<u8>) {
     }
 }
 
-fn perms_visited(mut orig: Vec<u8>, next_round: u8, visited: &HashSet<(u64, u8)>) -> bool {
+// This function modifies 'orig' but returns it unchanged when the return value is true.
+fn perms_visited(orig: &mut Vec<u8>, next_round: u8, visited: &HashMap<(u64, u8), Option<((u64, u8), u8)>>) -> bool {
     let n = N;
     let nnn = to_number(&orig);
-    if visited.contains(&(nnn, next_round)) {
+    if visited.contains_key(&(nnn, next_round)) {
         return true;
     }
     let mut perm: Vec<u8> = (0..(n as u8)).collect();
@@ -101,13 +130,14 @@ fn perms_visited(mut orig: Vec<u8>, next_round: u8, visited: &HashSet<(u64, u8)>
         }
         
         if !changed {
+            swapper(0, 1, orig);
             return false;
         }
         let k: usize = ((i as i8) + dirs[i as usize]) as usize;
 
         dirs.swap(i, k);
         perm.swap(i, k);
-        swapper(i, k, &mut orig);
+        swapper(i, k, orig);
 
         if (k == 0) || (k == n-1) || (perm[(k as i64 + (dirs[k] as i64)) as usize] > e) {
             dirs[k] = 0;
@@ -123,7 +153,7 @@ fn perms_visited(mut orig: Vec<u8>, next_round: u8, visited: &HashSet<(u64, u8)>
             }
         }
         let nnn = to_number(&orig);
-        if visited.contains(&(nnn, next_round)) {
+        if visited.contains_key(&(nnn, next_round)) {
             return true;
         }
     }
